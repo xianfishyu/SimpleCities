4bd81eb56fef151e337bcab9c35dbf0bc4bff0d7
xianfishyu <1371866605@qq.com>
2026-08-16 08:14:36 +0800

    添加：V3 场景根节点当前槽文件数

--- Scripts/Road/V3/RoadGraphV3System.cs
+++ Scripts/Road/V3/RoadGraphV3System.cs
@@ -28,6 +28,7 @@ public partial class RoadGraphV3System : Node2D
     public long? CurrentSlotPopulation => Application.CurrentSlotPopulation;
     public decimal? CurrentSlotFunds => Application.CurrentSlotFunds;
     public string? CurrentSlotThumbnailFile => Application.CurrentSlotThumbnailFile;
+    public int CurrentSlotFileCount => Application.CurrentSlotFileCount;
 
     public System.Collections.Generic.IReadOnlyList<SimpleCities.Core.V3.V3SlotSummary> ListUsableSlots() =>
         Application.ListUsableSlots();
