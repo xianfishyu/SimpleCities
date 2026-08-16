62df4cd90111616b098a7107d21aea864854c3c4
xianfishyu <1371866605@qq.com>
2026-08-16 08:12:08 +0800

    添加：V3 场景根节点当前槽人口与资金

--- Scripts/Road/V3/RoadGraphV3System.cs
+++ Scripts/Road/V3/RoadGraphV3System.cs
@@ -25,6 +25,8 @@ public partial class RoadGraphV3System : Node2D
     public bool CurrentSlotIsCorrupt => Application.CurrentSlotIsCorrupt;
     public SimpleCities.Core.V3.V3Manifest? CurrentSlotManifest => Application.CurrentSlotManifest;
     public string? CurrentSlotCityName => Application.CurrentSlotCityName;
+    public long? CurrentSlotPopulation => Application.CurrentSlotPopulation;
+    public decimal? CurrentSlotFunds => Application.CurrentSlotFunds;
 
     public System.Collections.Generic.IReadOnlyList<SimpleCities.Core.V3.V3SlotSummary> ListUsableSlots() =>
         Application.ListUsableSlots();
