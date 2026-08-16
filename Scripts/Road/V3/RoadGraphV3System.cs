7e9bb1f74d18ea333c3759c25e955c39fd34ed20
xianfishyu <1371866605@qq.com>
2026-08-16 08:23:38 +0800

    添加：V3 场景根节点当前槽外部标记

--- Scripts/Road/V3/RoadGraphV3System.cs
+++ Scripts/Road/V3/RoadGraphV3System.cs
@@ -25,6 +25,7 @@ public partial class RoadGraphV3System : Node2D
     public bool CurrentSlotIsCorrupt => Application.CurrentSlotIsCorrupt;
     public bool CurrentSlotIsAbsent => Application.CurrentSlotIsAbsent;
     public bool CurrentSlotIsUnsafe => Application.CurrentSlotIsUnsafe;
+    public bool CurrentSlotIsForeign => Application.CurrentSlotIsForeign;
     public SimpleCities.Core.V3.V3Manifest? CurrentSlotManifest => Application.CurrentSlotManifest;
     public string? CurrentSlotCityName => Application.CurrentSlotCityName;
     public long? CurrentSlotPopulation => Application.CurrentSlotPopulation;
