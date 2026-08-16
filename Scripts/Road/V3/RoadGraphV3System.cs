60d2b41d4b527d7b7022c0f8b860f8993fbaac4a
xianfishyu <1371866605@qq.com>
2026-08-16 08:02:20 +0800

    添加：V3 场景根节点当前槽显示名

--- Scripts/Road/V3/RoadGraphV3System.cs
+++ Scripts/Road/V3/RoadGraphV3System.cs
@@ -18,6 +18,7 @@ public partial class RoadGraphV3System : Node2D
     public void ClearHistory() => Application.ClearHistory();
     public string CurrentSlotID => Application.CurrentSlotID;
     public SimpleCities.Core.V3.V3SlotSummary? CurrentSlotSummary => Application.CurrentSlotSummary;
+    public string? CurrentSlotDisplayName => Application.CurrentSlotDisplayName;
 
     public System.Collections.Generic.IReadOnlyList<SimpleCities.Core.V3.V3SlotSummary> ListUsableSlots() =>
         Application.ListUsableSlots();
