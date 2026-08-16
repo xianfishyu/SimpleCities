ae039cc4543a788de660b597568e9fe3ad8f4e1a
xianfishyu <1371866605@qq.com>
2026-08-16 08:34:51 +0800

    添加：V3 场景根节点图统计

--- Scripts/Road/V3/RoadGraphV3System.cs
+++ Scripts/Road/V3/RoadGraphV3System.cs
@@ -13,6 +13,8 @@ public partial class RoadGraphV3System : Node2D
     public RoadGraphV3Application Application { get; private set; } = null!;
     public RoadGraphV3Controller Controller => Application.Controller;
     public RoadGraphV3Revision Revision => Application.Revision;
+    public int CurrentNodeCount => Application.CurrentNodeCount;
+    public int CurrentEdgeCount => Application.CurrentEdgeCount;
     public bool CanUndo => Application.CanUndo;
     public bool CanRedo => Application.CanRedo;
     public void ClearHistory() => Application.ClearHistory();
